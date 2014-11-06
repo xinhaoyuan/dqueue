ccb1ca02a71fcaf9cb433f8a7d64371b4fc73c81
Xinhao Yuan <xinhaoyuan@bug05.cs.columbia.edu>
2014-11-05 23:30:17 -0500

    .

--- generate.sh
+++ generate.sh
@@ -3,3 +3,5 @@
 CCFLAGS='-DLOCK=0 -DHELP_THRESHOLD=0' make clean test; mv test test_spin
 CCFLAGS='-DLOCK=1 -DHELP_THRESHOLD=0' make clean test; mv test test_0
 CCFLAGS='-DLOCK=1 -DHELP_THRESHOLD=1' make clean test; mv test test_1
+CCFLAGS='-DLOCK=1 -DHELP_THRESHOLD=2' make clean test; mv test test_2
+CCFLAGS='-DLOCK=1 -DHELP_THRESHOLD=5' make clean test; mv test test_5
